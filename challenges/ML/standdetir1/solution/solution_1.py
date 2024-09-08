c0a86fabcf46568bca00b307ae41b01a41077b2e
Florent Duchesne <florentduchesne@hotmail.com>
2024-09-08 19:14:27 -0400

    write-up + dossiers solution + test SVM et MLP

--- challenges/ML/standdetir1/solution/solution_1.py
+++ challenges/ML/standdetir1/solution/solution_1.py
@@ -1,10 +1,11 @@
 import csv
 from sklearn.neighbors import KNeighborsClassifier
-from utils import distance_euclide
-from generer_points_2 import score_point
+from sklearn.svm import SVC
+from generer_points_1 import distance_euclide, score_point
+from sklearn.neural_network import MLPClassifier
 
 #lit un fichier CSV
-def load_dataset(fichier, dim=3):
+def load_dataset(fichier):
     points = []
     scores = []
     with open(fichier, 'r') as f:
@@ -13,9 +14,8 @@ def load_dataset(fichier, dim=3):
         for line in reader:
             x = float(line[0])
             y = float(line[1])
-            z = float(line[2])
-            points.append((x, y, z))
-            if len(line) > dim:
+            points.append((x, y))
+            if 'train' in fichier:
                 scores.append(int(line[-1]))
     return points, scores
 
@@ -31,6 +31,12 @@ def knn(points_train, scores_train, points_test, k):
         scores_test.append(mode)
     return scores_test
 
+def svc_sklearn(points_train, scores_train, points_test):
+    svc = SVC()
+    svc.fit(points_train, scores_train)
+    scores_test = [svc.predict([pt])[0] for pt in points_test]
+    return scores_test
+
 #knn de sklearn
 def knn_sklearn(points_train, scores_train, points_test, k=3):
     knc = KNeighborsClassifier(n_neighbors=k)
@@ -38,26 +44,34 @@ def knn_sklearn(points_train, scores_train, points_test, k=3):
     scores_test = [knc.predict([pt])[0] for pt in points_test]
     return scores_test
 
-# Verite absolue
-def get_gt(points, centre_zones, centre_zones_scores):
+def mlp_sklearn(points_train, scores_train, points_test):
+    mlp = MLPClassifier()
+    mlp.fit(points_train, scores_train)
+    scores_test = [mlp.predict([pt])[0] for pt in points_test]
+    return scores_test
+
+def get_gt(points):
     scores = []
     for point in points:
-        score = score_point(point, centre_zones, centre_zones_scores)
+        score, dist = score_point(point)
         scores.append(score)
     return scores
 
 def main():
-    centre_zones, centre_zones_scores = load_dataset('../dataset/centres_zones_2.csv')
-    points_train, scores_train = load_dataset('../dataset/dataset_train_2.csv')
-    points_test, _ = load_dataset('../dataset/dataset_test_2.csv')
+    points_train, scores_train = load_dataset('../dataset/dataset_train_1.csv')
+    points_test, _ = load_dataset('../dataset/dataset_test_1.csv')
     print(len(points_test))
     scores_test = knn(points_train, scores_train, points_test, 3)
     scores_test2 = knn_sklearn(points_train, scores_train, points_test, 3)
+    scores_test3 = svc_sklearn(points_train, scores_train, points_test)
+    scores_test4 = mlp_sklearn(points_train, scores_train, points_test)
     print(scores_test)
     print(scores_test2)
-    scores_test_gt = get_gt(points_test, centre_zones, centre_zones_scores)
+    print(scores_test3)
+    print(scores_test4)
+    scores_test_gt = get_gt(points_test)
     print(scores_test_gt)
-    print(sum(scores_test), sum(scores_test2), sum(scores_test_gt))
+    print('Checksum:', sum(scores_test), sum(scores_test2), sum(scores_test3), sum(scores_test4), sum(scores_test_gt))
 
 if __name__ == '__main__':
     main()
