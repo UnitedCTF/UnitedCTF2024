2f0da8abd13504151c614cef749cd476f932fbfd
Julien Champoux <chaj2055@usherbrooke.ca>
2024-09-08 04:22:48 -0400

    i love bpf filters

--- challenges/pwn/shellcode-obfuscation/Source/exec_shellcode.c
+++ challenges/pwn/shellcode-obfuscation/Source/exec_shellcode.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <sys/mman.h>
+#include <linux/filter.h>
 #include <linux/seccomp.h>
+#include <sys/syscall.h>
 #include <string.h>
 #include <malloc.h>
 #include <linux/prctl.h>
@@ -26,8 +28,29 @@ int main(int argc, char **argv)
     void *page_start = (void *) ((long) payload & -page_size);
     mprotect(page_start, page_size * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
 
-    if(prctl(PR_SET_SECCOMP, SECCOMP_MODE_STRICT)) {
-        dprintf(2, "WARN: failed to enable seccomp\n");
+    struct sock_filter filter[] = {
+        BPF_STMT(BPF_LD | BPF_W | BPF_ABS, (offsetof(struct seccomp_data, nr))),
+        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_write, 0, 2),
+        BPF_STMT(BPF_LD | BPF_W | BPF_ABS, (offsetof(struct seccomp_data, args))),
+        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, 1, 2, 1),
+        BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_exit, 1, 0),
+        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_KILL),
+        BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW)
+    };
+
+    struct sock_fprog prog = {
+        .len = (unsigned short)(sizeof(filter) / sizeof(filter[0])),
+        .filter = filter
+    };
+
+    if(prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)) {
+        perror("set_no_new_privs failed");
+        return errno;
+    }
+
+    if(prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &prog)) {
+        perror("set_seccomp failed");
+        return errno;
     }
 
     asm(
