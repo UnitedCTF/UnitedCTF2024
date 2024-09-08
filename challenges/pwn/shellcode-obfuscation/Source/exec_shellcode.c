df4703f13ff458fb63f43dbee89f75b67def4fd7
Julien Champoux <chaj2055@usherbrooke.ca>
2024-09-08 02:55:08 -0400

    formatting + seccomp

--- challenges/pwn/shellcode-obfuscation/Source/exec_shellcode.c
+++ challenges/pwn/shellcode-obfuscation/Source/exec_shellcode.c
@@ -6,12 +6,15 @@
 #include <linux/prctl.h>
 #include <sys/prctl.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
     if(argc < 2) return 1;
 
     int arg1Length = strlen(argv[1]) - 1;
+    if(arg1Length > 720 * 4) return 1;
+
     int inputLength = arg1Length < 720 * 4 ? arg1Length : 720 * 4;
     unsigned char* payload = malloc(inputLength / 4);
     for(int i = 0; i < inputLength; i += 4) {
@@ -22,7 +25,10 @@ int main(int argc, char **argv)
     long page_size = sysconf(_SC_PAGESIZE);
     void *page_start = (void *) ((long) payload & -page_size);
     mprotect(page_start, page_size * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
-    prctl(PR_SET_SECCOMP, SECCOMP_MODE_STRICT);
+
+    if(prctl(PR_SET_SECCOMP, SECCOMP_MODE_STRICT)) {
+        dprintf(2, "WARN: failed to enable seccomp\n");
+    }
 
     asm(
             "call %0"
